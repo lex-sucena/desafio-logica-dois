29234b365690b11742c862ab63c3f58d6d71aab8
lex-sucena <alex.everythingelse@gmail.com>
2023-12-05 10:47:13 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let nivel;
+function calcSaldo(vitorias, derrotas) {
+  let saldo = vitorias - derrotas;
+  return saldo;
+}
+let saldoVitorias = calcSaldo(44, 22);
+if (10 > saldoVitorias) {
+  nivel = "Ferro";
+} else if (saldoVitorias > 10 && saldoVitorias < 20) {
+  nivel = "Bronze";
+} else if (saldoVitorias > 20 && saldoVitorias < 50) {
+  nivel = "Prata";
+} else if (saldoVitorias > 50 && saldoVitorias < 80) {
+  nivel = "Ouro";
+} else if (saldoVitorias > 80 && saldoVitorias < 90) {
+  nivel = "Diamante";
+} else if (saldoVitorias > 90 && saldoVitorias < 100) {
+  nivel = "Lendário";
+} else if (saldoVitorias >= 101) {
+  nivel = "Imortal";
+}
+console.log(
+  `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`
+);
